9b2916dd34faa91e0566360a44f1a9ef059f87bd
bhabgs <bhabgs@aliyun.com>
2020-02-12 10:32:26 +0800

    删除log

--- src/views/editor/components/code_edit.tsx
+++ src/views/editor/components/code_edit.tsx
@@ -1,4 +1,4 @@
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Ref } from 'vue-property-decorator';
 
 @Component
 export default class CodeEdit extends Vue {
@@ -7,19 +7,28 @@ export default class CodeEdit extends Vue {
   })
   private value?: any;
 
+  private Editor: any = null;
+
   private mounted() {
     const { value } = this;
-    this.$Editor.editor.create(document.getElementById('code_edit'), {
-      language: 'typescript',
-      theme: 'vs', // -dark
-      value,
-      automaticLayout: true,
-      // 代码略缩图
-      minimap: {
-        enabled: false,
+    this.Editor = this.$Editor.editor.create(
+      document.getElementById('code_edit'),
+      {
+        language: 'typescript',
+        theme: 'vs', // -dark
+        value,
+        automaticLayout: true,
+        // 代码略缩图
+        minimap: {
+          enabled: false,
+        },
+        readOnly: false,
       },
-      readOnly: false,
-    });
+    );
+  }
+
+  private getvalue() {
+    return this.Editor.getValue();
   }
 
   private render() {
