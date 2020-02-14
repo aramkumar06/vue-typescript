1d84347b3c593b5f04771f3879856c6283df61dd
wpp <906008377@qq.com>
2020-02-14 16:01:15 +0800

    解决冲突

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -3,26 +3,16 @@
  * @version: 0.0.1
  * @Author: bhabgs
  * @Date: 2020-01-16 12:16:20
-<<<<<<< HEAD
  * @LastEditors  : wpp
- * @LastEditTime : 2020-02-14 15:38:19
-=======
- * @LastEditors  : bhabgs
- * @LastEditTime : 2020-02-14 11:03:58
->>>>>>> 0b3c2a3ff925f13b6926e5ebc9f358baead93c42
+ * @LastEditTime : 2020-02-14 16:00:54
  */
 import { MutationTree } from 'vuex';
 import { State } from './types';
 import { layout } from '../utils/baseData/type';
 
 const mutations: MutationTree<State> = {
-<<<<<<< HEAD
   PAGE_CONTENT_ADD_LAYOUT(state: State, layoutComponent: any) {
     state.pageContent.children.push(layoutComponent);
-=======
-  PAGE_CONTENT_ADD_LAYOUT(state: State, layout: any) {
-    state.pageContent.children.push(layout);
->>>>>>> 0b3c2a3ff925f13b6926e5ebc9f358baead93c42
   },
 
   LAYOUT_CHILDREN_ADD_COMPONENT(state: State, opt: any) {
