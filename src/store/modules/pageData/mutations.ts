910f064521a5622f96da9df76f4182633ed21bbc
bhabgs <bhabgs@aliyun.com>
2020-03-09 14:22:47 +0800

    优化删除方法

--- src/store/modules/pageData/mutations.ts
+++ src/store/modules/pageData/mutations.ts
@@ -2,11 +2,12 @@
  * @Autor: wpp
  * @Date: 2020-02-17 14:14:20
  * @LastEditors: bhabgs
- * @LastEditTime: 2020-03-09 10:32:25
+ * @LastEditTime: 2020-03-09 14:19:53
  */
 
 import { MutationTree } from 'vuex';
 import { component } from '@/store/pageType';
+import { deepCopy } from '@/utils';
 import { State } from './type';
 
 const lzString = require('lz-string');
@@ -23,7 +24,9 @@ const addToChildrenFn = (
 ) => {
   for (const item of arr) {
     if (item.id === opt.parent.id) {
-      const id = opt.parent.treeId || opt.parent.id;
+      const id = opt.parent.treeId
+        ? lzString.decompress(opt.parent.treeId)
+        : opt.parent.id;
       const newData = Object.assign(opt.component, {
         treeId: lzString.compress(`${id}-${opt.component.id}`),
       });
@@ -35,14 +38,76 @@ const addToChildrenFn = (
   }
 };
 
+// 记录删除时每一层及的key
+let treeIds: number[] = [];
+const getKeyTree = (who: any[], id: string) => {
+  let n: number = 0;
+  for (const i of who) {
+    n += 1;
+    if (i.id === id) {
+      treeIds.push(n - 1);
+    }
+    if (i.children) {
+      getKeyTree(i.children, id);
+    }
+  }
+};
+
 // 获取操作位置
 const getActiveData = (who: any[], id: string) => {
+  let n: number = 0;
   for (const i of who) {
+    n += 1;
     if (i.id === id) {
       pageData = i;
+      treeIds.push(n - 1);
     }
     if (i.children) {
       getActiveData(i.children, id);
+    }
+  }
+};
+// 根据id获取当前下标 tress 数组进行优化递归查询
+const getKeyById = (id: string, trees: number[]) => {
+  // let keyId = 0;
+  // 保留代码优化递归方案
+  // if (trees.length <= 0) {
+  //   keyId = 0;
+  // } else {
+  //   // 查询到已知key
+  //   for (const i in trees) {
+  //     if (Number(i) !== 0) {
+  //       pageData = pageData.children[trees[i]];
+  //     }
+  //   }
+  //   console.log(pageData);
+  //   // 设置未设置的key 位置
+  //   for (const i in pageData.children) {
+  //     // console.log(id);
+  //     if (pageData.children[i].id === id) {
+  //       keyId = Number(i);
+  //     }
+  //   }
+  // }
+  // return keyId;
+};
+
+// 删除
+const remove = (id: string) => {
+  const ids = lzString.decompress(id).split('-');
+  const useId = ids.splice(ids.length - 1, 1);
+  treeIds = [];
+  for (const i in ids) {
+    getKeyTree(deepCopy(pageData.children), ids[i]);
+  }
+
+  for (const i in treeIds) {
+    pageData = pageData.children[treeIds[i]];
+  }
+
+  for (const i in pageData.children) {
+    if (pageData.children[i].id === useId[0]) {
+      pageData.children.splice(i, 1);
     }
   }
 };
@@ -69,7 +134,11 @@ const mutations: MutationTree<State> = {
   },
   // 删除某一个组件 或者布局
   REMOVE_WHO(state: State, id: string) {
-    console.log(lzString.decompress(id));
+    pageData = state.pageContent;
+    remove(id);
+    setTimeout(() => {
+      state.activeData = pageData;
+    }, 100);
   },
 };
 export default mutations;
