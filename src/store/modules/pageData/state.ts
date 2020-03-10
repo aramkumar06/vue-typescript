2e7f8326505f27da2f0d7e8cc5b78cd9a1f03575
bhabgs <bhabgs@aliyun.com>
2020-03-10 11:06:38 +0800

    添加数据源数据结构页面

--- src/store/modules/pageData/state.ts
+++ src/store/modules/pageData/state.ts
@@ -2,7 +2,7 @@
  * @Autor: wpp
  * @Date: 2020-02-17 14:14:20
  * @LastEditors: bhabgs
- * @LastEditTime: 2020-03-02 17:16:24
+ * @LastEditTime: 2020-03-10 09:48:18
  */
 import { State } from './type';
 
@@ -12,6 +12,8 @@ const state: State = {
     id: 'page',
     children: [],
     event: [],
+    sourceData: {},
+    editData: [],
   },
   activeKey: '',
   activeData: {},
